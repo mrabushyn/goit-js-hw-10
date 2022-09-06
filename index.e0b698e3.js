55dd172a48bb5dff5ed0c5a41fa6d0d4278eef3d
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-09-06 18:03:43 +0000

    Deploying to gh-pages from @ mrabushyn/goit-js-hw-10@2b0b775189947252b371791a9a95e51dd9147a87 🚀

--- index.e0b698e3.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/{name}").then((e=>e.json())).then(console.log(result)).catch(error).finally(console.log(result));
-//# sourceMappingURL=index.e0b698e3.js.map
