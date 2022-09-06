8a310b69bec140eb4b2b9c5c338a64b106e718bc
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-09-06 18:24:12 +0300

    2

--- src/fetchCountries.js
+++
@@ -1,3 +0,0 @@
-// const url = https://restcountries.com/v3.1/name/{name}
-
-// export  fetch ('https://restcountries.com/v3.1/name') 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import './css/styles.css';
-import './src/fetchCountries.js';
+// import './src/fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
 
-fetch('https://restcountries.com/v3.1/name');
+// console.log(32132123)
+
+fetchCountries('ukraine')
+  .then(console.log(result))
+  .catch(error)
+  .finally(console.log(result)); 
+
+
+function fetchCountries (name) {
+    const url = `https://restcountries.com/v3.1/name/{name}`;
+    
+    return fetch(url).then(r => r.json());
+}
