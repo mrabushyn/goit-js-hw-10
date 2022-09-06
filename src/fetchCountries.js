f0d20b89cd815848123008fb9b147e674d10b36b
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-09-06 18:01:51 +0300

    1

---
+++ src/fetchCountries.js
@@ -0,0 +1,3 @@
+// const url = https://restcountries.com/v3.1/name/{name}
+
+// export  fetch ('https://restcountries.com/v3.1/name') 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './css/styles.css';
+import './src/fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
+
+fetch('https://restcountries.com/v3.1/name');
