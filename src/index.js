134eba8dfc35563df217192f84f4b628bdec4e67
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-09-08 22:11:04 +0300

    hidden

--- src/index.js
+++ src/index.js
@@ -5,60 +5,88 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
+  body: document.querySelector('body'),
+  // body2: document.querySelector('div'),
   inputField: document.querySelector('input'),
   coutryList: document.querySelector('.country-list'),
   coutryInfo: document.querySelector('.country-info'),
 };
-refs.inputField.addEventListener('input', debounce(onEnterCountryName, DEBOUNCE_DELAY));
+
+// console.log(refs.body);
+// console.log(refs.body2);
+
+refs.inputField.addEventListener(
+  'input',
+  debounce(onEnterCountryName, DEBOUNCE_DELAY)
+);
+
+function fetchCountries(countryName) {
+  const url = `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`;
+  return fetch(url).then(response => response.json());
+}
 
 function onEnterCountryName(event) {
-  const countryName = event.target.value;
-
-fetchCountries(countryName)
-  .then(renderCountryList)
-  .catch(onError)
-//   .finally(console.log(countryName));
+  const countryName = event.target.value.trim();
+  fetchCountries(countryName).then(render).catch(onError);
+  // .finally(countryName => countryName.reset());
 }
 
-function fetchCountries(name) {
-  const url = `https://restcountries.com/v3.1/name/${name}`;
-  return fetch(url).then(r => r.json());
-}
-
-function onError(error) {
-  console.log('EERRRROORR');
+function render(countryName) {
+  if (countryName.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  } else if (countryName.length === 1) {
+    refs.coutryList.classList.add('is-hidden');
+    renderCountryInfo(countryName);
+    // console.log(refs.body);
+    // console.log(refs.body2);
+  } else if (countryName.length < 10) {
+    renderCountryList(countryName);
+    // console.log(refs.body);
+    // console.log(refs.body2);
+  }
 }
 
 function renderCountryList(countryName) {
-    console.log(countryName);
-
-  const marcupList =
-
-    `${countryName.name.official}` +
-    '     ' +
-    `${countryName.capital}` +
-    '     ' +
-    `${countryName.population}` +
-    '     ' +
-    `${countryName.flags.svg}` +
-    '     ' +
-    `${countryName.languages}`;
-
-
-//   const marcupInfo =
-
-//     `${countryName[0].name.official}` +
-//     '     ' +
-//     `${countryName[0].capital}` +
-//     '     ' +
-//     `${countryName[0].population}` +
-//     '     ' +
-//     `${countryName[0].flags.svg}` +
-//     '     ' +
-//     `${countryName[0].languages}`;
-
-
-  refs.coutryList.innerHTML = marcupList;
-//   refs.coutryInfo.innerHTML = marcupInfo;
-
+  for (let i = 0; i < countryName.length; i += 1) {
+    const flag1 = countryName[i].flags.svg;
+    const name1 = countryName[i].name.common;
+    const marcupList = `<li>
+        <img src = ${flag1} alt = "" width="35"></img>
+                <span>${name1}</span>
+      </li>`;
+    refs.coutryList.insertAdjacentHTML('beforeend', marcupList);
+  }
 }
+
+function renderCountryInfo(countryName) {
+  for (let i = 0; i < countryName.length; i += 1) {
+    const flag2 = countryName[0].flags.svg;
+    const name = countryName[0].name.common;
+    const capital = countryName[0].capital;
+    const population = countryName[0].population;
+    const language = Object.values(countryName[0].languages);
+    const marcupInfo = `<li>
+        <img src = ${flag2} alt = "" width="35">${name}</img>
+        <p>Capital: ${capital}</p>
+        <p>Population: ${population}</p>
+        <p>Language: ${language}</p>
+      </li>`;
+    refs.coutryList.insertAdjacentHTML('beforeend', '');
+    refs.coutryInfo.insertAdjacentHTML('beforeend', marcupInfo);
+  }
+}
+
+function onError(error) {
+  console.log('ERRRRRRRRRRRRRRR', error);
+  Notify.failure('"Oops, there is no country with that name"');
+}
+
+refs.body.insertAdjacentHTML(
+  'beforebegin',
+  `<style>
+    .is-hidden {
+      visibility: hidden;
+      margin: 0
+    } 
+    </style>`
+);
