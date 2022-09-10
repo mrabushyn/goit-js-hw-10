818ec8be4cd8cd281dfc7a8408ce4064edc56bea
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-09-10 17:10:39 +0300

    finish

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+export { fetchCountries };
+
+function fetchCountries(countryName) {
+  const url = `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`;
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,96 +1,76 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import './src/fetchCountries.js';
+import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
   body: document.querySelector('body'),
-  // body2: document.querySelector('div'),
   inputField: document.querySelector('input'),
   coutryList: document.querySelector('.country-list'),
   coutryInfo: document.querySelector('.country-info'),
 };
-
-// console.log(refs.body);
-// console.log(refs.body2);
 
 refs.inputField.addEventListener(
   'input',
   debounce(onEnterCountryName, DEBOUNCE_DELAY)
 );
 
-function fetchCountries(countryName) {
-  const url = `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`;
-  return fetch(url).then(response => response.json());
-}
-
 function onEnterCountryName(event) {
-  const countryName = event.target.value.trim();
-
-  fetchCountries(countryName).then(render).catch(onError)
+  let countryName = event.target.value;
 
   refs.coutryList.classList.remove('is-hidden')
   refs.coutryInfo.classList.remove('is-hidden')
-  
+  refs.coutryList.innerHTML = "";
+  refs.coutryInfo.innerHTML = "";
 
-  // (countryName => countryName.reset());
-  // countryName.reset();
+  if (event.target.value === '') {
+ return
+  }
+
+  fetchCountries(countryName.trim()).then(countryName => render(countryName))
+  .catch(onError)
+
+function onError(error) {
+  Notify.failure('"Oops, there is no country with that name"');
 }
 
 function render(countryName) {
   if (countryName.length > 10) {
     Notify.info('Too many matches found. Please enter a more specific name.');
-    refs.coutryList.classList.add('is-hidden');
-    refs.coutryInfo.classList.add('is-hidden');
-  } else if (countryName.length === 1) {
-    refs.coutryList.classList.add('is-hidden');
+    return;
+ } else {
+listOrInfo (countryName)
+}}
+
+function listOrInfo (countryName) {
+  if (countryName.length === 1) {
     renderCountryInfo(countryName);
-  } else if (countryName.length < 10) {
-    renderCountryList(countryName);
-    refs.coutryInfo.classList.add('is-hidden');
-  } else if (countryName.length = 0) {
-    refs.coutryList.classList.add('is-hidden');
-    refs.coutryInfo.classList.add('is-hidden');
-  }
-}
+  } else {
+  renderCountryList(countryName);
+}}
 
 function renderCountryList(countryName) {
   for (let i = 0; i < countryName.length; i += 1) {
-    const flag1 = countryName[i].flags.svg;
-    const name1 = countryName[i].name.common;
     const marcupList = `<li>
-        <img src = ${flag1} alt = "" width="35"></img>
-                <span class="nameCountryList">${name1}</span>
+        <img src = ${countryName[i].flags.svg} alt = "${countryName[i].name.common}" width="35"></img>
+                <span class="nameCountryList">${countryName[i].name.common}</span>
       </li>`;
-    refs.coutryInfo.insertAdjacentHTML('beforeend', '');
     refs.coutryList.insertAdjacentHTML('beforeend', marcupList);
   }
 }
 
 function renderCountryInfo(countryName) {
-  for (let i = 0; i < countryName.length; i += 1) {
-    const flag2 = countryName[0].flags.svg;
-    const name = countryName[0].name.common;
-    const capital = countryName[0].capital;
-    const population = countryName[0].population;
-    const language = Object.values(countryName[0].languages);
     const marcupInfo = `<div>
-        <img  src = ${flag2} alt = "" width="35"></img>
-        <span class="nameCountryInfo">${name}</span>
-        <p>Capital: ${capital}</p>
-        <p>Population: ${population}</p>
-        <p>Language: ${language}</p>
+        <img src = ${countryName[0].flags.svg} alt = "${countryName[0].name.common}" width="35"></img>
+        <span class="nameCountryInfo">${countryName[0].name.common}</span>
+        <p>Capital: ${countryName[0].capital}</p>
+        <p>Population: ${countryName[0].population}</p>
+        <p>Language: ${Object.values(countryName[0].languages)}</p>
       </div>`;
-    refs.coutryList.insertAdjacentHTML('beforeend', '');
     refs.coutryInfo.insertAdjacentHTML('beforeend', marcupInfo);
   }
-}
 
-function onError(error) {
-  console.log('ERRRRRRRRRRRRRRR', error);
-  Notify.failure('"Oops, there is no country with that name"');
-}
 
 refs.body.insertAdjacentHTML(
   'beforebegin',
@@ -116,4 +96,4 @@ font-size: 24px;
       margin-left: 10px;
     }
     </style>`
-);
+)
